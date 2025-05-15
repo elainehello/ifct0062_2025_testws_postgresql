e4769a786da58808aba6a55a1faf454fb982ddda
elainehello <195331169+elainehello@users.noreply.github.com>
2025-05-15 19:38:04 +0200

    --

--- src/main/java/com/elainevalles/getafe/ifct0062_2025_testws_postgresql/Repository/IMovieRepository.java
+++ src/main/java/com/elainevalles/getafe/ifct0062_2025_testws_postgresql/Repository/IMovieRepository.java
@@ -1,5 +1,9 @@
 package com.elainevalles.getafe.ifct0062_2025_testws_postgresql.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import com.elainevalles.getafe.ifct0062_2025_testws_postgresql.model.Movie;
+
 @RepositoryRestResource
 public interface IMovieRepository extends JpaRepository<Movie, Integer> {
 }
