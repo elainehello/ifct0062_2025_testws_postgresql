965c86f994285bbe0ac80fe39267f65fd4407b94
elainehello <195331169+elainehello@users.noreply.github.com>
2025-05-15 16:42:14 +0200

    Initial commit

---
+++ src/main/java/com/elainevalles/getafe/ifct0062_2025_testws_postgresql/Ifct00622025TestwsPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.elainevalles.getafe.ifct0062_2025_testws_postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ifct00622025TestwsPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ifct00622025TestwsPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elainevalles/getafe/ifct0062_2025_testws_postgresql/Ifct00622025TestwsPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elainevalles.getafe.ifct0062_2025_testws_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ifct00622025TestwsPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
