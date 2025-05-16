e6c1cdce47b4fb7eb10d4edd6a7f570334400a40
elainehello <195331169+elainehello@users.noreply.github.com>
2025-05-16 20:51:14 +0200

    --

--- src/main/java/com/elainevalles/getafe/ifct0062_2025_testws_postgresql/model/Movie.java
+++ src/main/java/com/elainevalles/getafe/ifct0062_2025_testws_postgresql/model/Movie.java
@@ -9,12 +9,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+/**
+ * Lombok giving parsing issues, therefore may be useful change to a normal JavaBean without annotations from lombok
+ */
 
+@Entity
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +23,53 @@ public class Movie {
     private int year;
     private String poster;
 
+    public Movie() {
+    }
+
+    public Movie(String title, String director, int year, String poster) {
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.poster = poster;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
 }
